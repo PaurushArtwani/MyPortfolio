c98718e5d8ce8c96cc28e4422ede1ac2b63cffa7
paurushbhai <55234702+paurushbhai@users.noreply.github.com>
2023-10-13 22:34:37 +0530

    Footer Section Started
    Footer Section Started

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,29 @@
+import { Col, Container, Row } from "react-bootstrap";
 import HeaderSection from "../HeaderSection/HeaderSection";
+import styled from "styled-components";
 
 const Footer = () => {
   return (
     <Container>
       <HeaderSection header={"Contact Me"}></HeaderSection>
+
+      <Row>
+        <Col>
+          <div className="icons-section">
+            <div>
+              <a href="#">
+                <Logo src="gmail.png" style={{ height: "30px" }} />
+              </a>
+            </div>
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 };
+const Link = styled.a``;
+const Logo = styled.img`
+  width: 50px;
+  height: 50px;
+`;
 export default Footer;
