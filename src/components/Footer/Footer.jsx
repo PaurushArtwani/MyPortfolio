5ef3a19132e6cd6818fd784fd3328b6b2fd13f02
paurushbhai <55234702+paurushbhai@users.noreply.github.com>
2023-10-15 13:36:35 +0530

    Footer Section Updated
    Footer Section Updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,21 +9,66 @@ const Footer = () => {
 
       <Row>
         <Col>
-          <div className="icons-section">
-            <div>
-              <a href="#">
-                <Logo src="gmail.png" style={{ height: "30px" }} />
-              </a>
-            </div>
-          </div>
+          <Logo className="icons-section">
+            <a className="socials">
+              <img src="gmail.png" />
+            </a>
+            <a className="socials">
+              <img src="phone.png" />
+            </a>
+            <a className="socials">
+              <img src="linkdin.png" />
+            </a>
+            <a className="socials">
+              <img src="instagram.png" />
+            </a>
+            <a className="socials">
+              <img src="facebook.png" />
+            </a>
+
+            <a className="socials">
+              <img src="twitter.png" />
+            </a>
+          </Logo>
+        </Col>
+      </Row>
+      <Row className="mt-5 mb-3">
+        <Col>
+          <FooterLabel>
+            <p>Copyright © Paurush Artwani</p>
+          </FooterLabel>
         </Col>
       </Row>
     </Container>
   );
 };
-const Link = styled.a``;
-const Logo = styled.img`
-  width: 50px;
-  height: 50px;
+
+const Logo = styled.div`
+  text-align: center;
+  .socials {
+    position: relative;
+    display: inline-block;
+    width: 90px;
+    height: 90px;
+    text-align: center;
+    padding: 15px;
+    transition: all 0.6s ease 0s;
+    margin-right: 30px;
+    margin-left: 30px;
+  }
+  img {
+    height: 100%;
+    width: 100%;
+  }
+  .socials:hover {
+    transform: translate(0, -10px);
+  }
+`;
+const FooterLabel = styled.div`
+  text-align: center;
+  p {
+    color: #db9a64;
+    font-size: 18px;
+  }
 `;
 export default Footer;
